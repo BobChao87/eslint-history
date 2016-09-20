68c9b627eb0a5cbe8ecdacec2d9841a4b6d3ba1c
Bob Chao <bobchao87@gmail.com>
2016-09-20 02:54:34 -0700

    Make buffers parse correctly.

--- src/svn/index.js
+++ src/svn/index.js
@@ -1,16 +1,27 @@
 (function() {
   let execSync = require('child_process').execSync;
 
+  /**
+   * @function update
+   *
+   * @description
+   *
+   * Fetches the desired revision down. Regularly used with successive version
+   * numbers in order to get a complete record of rules violations.
+   *
+   * @param {integer|string} [revision = 'HEAD'] Revision to check out.
+   * @return {List.<string>} List of changed files (WIP)
+   */
   function update(revision = 'HEAD') {
     try {
-      return execSync(`echo "svn up -r${revision}"`);
+      return execSync(`svn up -r${revision}`).toString();
     } catch (err) {
       return 'err';
     }
   }
 
   function log(start = 1, stop = 'HEAD') {
-    return execSync(`svn up -r${start}:${stop}`);
+    return execSync(`svn up -r${start}:${stop}`).toString();
   }
 
   var svn = {
