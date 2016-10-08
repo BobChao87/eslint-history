50d6a4140f7790eb9288c59690553531ca7ba7a5
BobChao87 <bobchao87@gmail.com>
2016-10-08 16:19:35 -0700

    Some basic arg inputs and start of arg parsing.

--- src/bin/index.js
+++ src/bin/index.js
@@ -3,9 +3,55 @@
 var svn = require('../svn');
 
 const validArgs = {
-
+  directory: {
+    required: false,
+    default: '.',
+    position: 2,
+    short: 'd',
+    takesArg: true,
+    requiredArg: true
+  },
+  start: {
+    required: false,
+    default: 'NEXT',
+    position: 3,
+    short: 's',
+    takesArg: true,
+    requiredArg: false
+  },
+  end: {
+    required: false,
+    default: 'HEAD',
+    position: 4,
+    short: 'e',
+    takesArg: true,
+    requiredArg: true
+  },
+  help: {
+    required: false,
+    default: null,
+    position: null,
+    short: 'h',
+    takesArg: false,
+    requiredArg: false
+  }
 };
 
 var passedArgs = {};
+
+// Tracks stuff
+var ignoreThis = false;
+process.argv.forEach(function processArg(arg, position, argv) {
+  console.log(arg, position, argv);
+  if (position < 2 || ignoreThis) {
+    ignoreThis = false;
+    // The first two are node and the file name. Ignore them.
+    return;
+  }
+});
+
+for (let arg in validArgs) {
+
+}
 
 console.log(validArgs, passedArgs);
