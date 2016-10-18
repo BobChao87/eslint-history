00eb9b8669b8230fa2d2b70438417930548b1c45
BobChao87 <bobchao87@gmail.com>
2016-10-17 19:43:36 -0700

    Demonstration of wait for linting idea
    Uses manual checking to advance instead of linting

--- src/bin/index.js
+++ src/bin/index.js
@@ -2,6 +2,40 @@
 
 var svn = require('../svn');
 
+var readline = require('readline');
+
 const validArgs = require('./validArgs.json');
 
 var parsedArgs = require('./parseArgs')(validArgs);
+
+var logs = svn.log(parsedArgs.start, parsedArgs.end);
+
+function recursiveFetch(revision, end) {
+  var files = svn.update(revision);
+  console.log(`Fetched revision ${revision}:`);
+  for (let file of files) {
+    console.log('\t', file);
+  }
+  var log = logs[revision];
+  console.log(`
+SVN Commit: ${log.revision}
+Author: ${log.author}
+
+${log.commitMessage}
+`
+  );
+  if (revision < end) {
+    let rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question('Hit ENTER to fetch next revision ', () => {
+      rl.close();
+      recursiveFetch(revision + 1, end);
+    });
+  } else {
+    console.log('Final revision found');
+  }
+}
+
+recursiveFetch(parseInt(parsedArgs.start, 10), Math.max(...Object.keys(logs)));
