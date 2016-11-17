d637cf8a83833ee24186e8a3d8aefb8cb21b0929
BobChao87 <bobchao87@gmail.com>
2016-11-16 19:52:48 -0800

    Beginning of ESLint exposed interface.
    Successfully tested against a real code base!

--- src/eslint/index.js
+++ src/eslint/index.js
@@ -1,7 +1,30 @@
 (function() {
   const execSync = require('child_process').execSync;
+  const parse = require('./parse');
 
-  const eslint = {};
+  /**
+   * @function runEslint
+   *
+   * @description
+   *
+   * Runs ESLint over the code base and passes the information through the
+   * ESLint parser.
+   *
+   * @return {Object[]} Computer readable format ESLint output.
+   */
+  function runEslint() {
+    // ESLint exits with a non-zero exit code when it detects errors
+    // (and warnings?). So we call `exit 0` here to prevent that.
+    var eslint = execSync('eslint . ; exit 0')
+      .toString();
+    return parse(eslint);
+  }
+
+  const eslint = {
+    run: runEslint
+  };
+
+  console.log(JSON.stringify(runEslint(), null, '  '));
 
   module.exports = eslint;
 })();
