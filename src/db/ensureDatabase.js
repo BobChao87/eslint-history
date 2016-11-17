e6dc9792f9969023d6907a9f83601dc270bc9b27
BobChao87 <bobchao87@gmail.com>
2016-11-17 03:22:19 -0800

    More complex database creation.
    Read database details from schema.
    Dynamic value substitution.
    
    Version bump.

--- src/db/ensureDatabase.js
+++ src/db/ensureDatabase.js
@@ -1,18 +1,58 @@
 (function() {
   const schema = require('./schema');
+  const KW = require('./keywords');
 
   var sqlite3 = require('sqlite3');
   var db = new sqlite3.Database('eslint_history');
 
-  for (let tableName of Object.keys(schema)) {
-    let columns = Object.keys(schema[tableName]);
+  function makeColumn(column, data) {
+    var columnString = [column];
+    var dataKeys = Object.keys(data);
+
+    if (dataKeys.indexOf(KW.TYPE) > -1) {
+      columnString.push(data[KW.TYPE]);
+    }
+    if (dataKeys.indexOf(KW.PROPERTIES) > -1) {
+      let properties = data[KW.PROPERTIES];
+      for (let property in properties) {
+        if (properties.hasOwnProperty(property)) {
+          let constraint = property.replace('{}', properties[property]);
+          columnString.push(constraint);
+        }
+      }
+    }
+    return columnString.join(' ');
+  }
+
+  function makeColumns(columns) {
+    var columnStrings = [];
+    for (let column in columns) {
+      if (columns.hasOwnProperty(column)) {
+        columnStrings.push(makeColumn(column, columns[column]));
+      }
+    }
+    return columnStrings.join(', ');
+  }
+
+  function makeTable(table, columns) {
     db.run(
       `CREATE TABLE IF NOT EXISTS
-        ${tableName}
+        ${table}
         (
-          ${columns.join(', ')}
+          ${makeColumns(columns)}
         )
       `
     );
   }
+
+  function makeDatabase() {
+    for (let tableName of Object.keys(schema)) {
+      let columns = schema[tableName];
+      makeTable(tableName, columns);
+    }
+  }
+
+  makeDatabase();
+
+  module.exports = makeDatabase;
 })();
