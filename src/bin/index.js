fe738034a46e78e501bfb9972dc92d23841e694a
BobChao87 <bobchao87@gmail.com>
2016-11-18 17:36:10 -0800

    Can fetch svn and run ESLint

--- src/bin/index.js
+++ src/bin/index.js
@@ -6,3 +6,26 @@ var db = require('../db');
 
 const validArgs = require('./validArgs.json');
 var parsedArgs = require('./parseArgs')(validArgs);
+
+/**
+ * @function main
+ *
+ * @description
+ *
+ * Does the ESLint
+ *
+ * @param {Object} args Key-value pairs of passed arguments
+ */
+function main(args) {
+  var start = Number(args.start);
+  var end = Number(args.end);
+
+  for (let revision = start; revision <= end; revision++) {
+    svn.update(revision);
+    let esout = eslint.eslint();
+    esout = eslint.lineInfo(esout);
+    console.log(revision);
+  }
+}
+
+main(parsedArgs);
