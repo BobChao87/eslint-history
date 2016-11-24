2130a198ee0e14d1bcde1902c79c8ab58eee7849
BobChao87 <bobchao87@gmail.com>
2016-11-23 18:26:58 -0800

    Progress on making output into db format

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
+var path = require('path');
+
 var svn = require('../svn');
 var eslint = require('../eslint');
+
 var db = require('../db');
+const schema = require('../db/schema');
 
 const validArgs = require('./validArgs.json');
 var parsedArgs = require('./parseArgs')(validArgs);
@@ -17,14 +21,32 @@ var parsedArgs = require('./parseArgs')(validArgs);
  * @param {Object} args Key-value pairs of passed arguments
  */
 function main(args) {
+  svn.directory = args.directory;
+
   var start = Number(args.start);
   var end = Number(args.end);
+
+  // It always excludes the trailing slash
+  var basePathLength = path.resolve(svn.directory).length + 1;
 
   for (let revision = start; revision <= end; revision++) {
     svn.update(revision);
     let esout = eslint.eslint();
     esout = eslint.lineInfo(esout);
-    console.log(revision);
+    console.log(`Revision ${revision}: ${esout[esout.length - 2].file}`);
+    for (let file of esout) {
+      if (!file.issues.length) {
+        continue;
+      }
+      console.log(`${file.file.substring(basePathLength)}`);
+      for (let issue of file.issues) {
+        let data = {};
+        for (let table of Object.keys(schema)) {
+          data[table] = {};
+        }
+        console.log(JSON.stringify(data, null, '  '));
+      }
+    }
   }
 }
 
